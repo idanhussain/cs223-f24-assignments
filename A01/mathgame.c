43eb05ba164bcbd5f8f1a294e1d487d4679a21f4
Idan Hussain <ihussain@goldengate.cs.brynmawr.edu>
2024-09-06 14:24:09 -0400

    final push

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,36 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Idan Hussain 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+	printf("Welcome to Math Game!\nHow many rounds do you want to play? ");
+	
+	int rounds = 0;
+	scanf("%d", &rounds);
+	int score = 0;
+
+	for (int i = 0; i < rounds; i++) {
+		int operand1 = (rand() % 9) + 1;
+		int operand2 = (rand() % 9) + 1;
+
+		printf("%d + %d = ", operand1, operand2);
+		
+		int response = 0;
+		scanf("%d", &response);
+
+		int correctAnswer = operand1 + operand2;
+		
+		if (response == correctAnswer) {
+			printf("Correct!\n");
+			score++;
+		} else printf("Incorrect :(\n");
+	}
+
+	printf("You answered %d/%d correctly.", score, rounds);
   return 0;
 }
