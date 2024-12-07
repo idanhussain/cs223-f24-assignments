5b4e0215f9aadb061eef0bc117daf4fd36c8fcbf
Idan Hussain <idan.hussain@gmail.com>
2024-12-07 16:06:18 -0500

    assignment complete

--- A12/mylloc_list.c
+++ A12/mylloc_list.c
@@ -1,20 +1,72 @@
+/*----------------------------------------------
+ * Author: Idan Hussain
+ * Date: December 6 2024
+ * This program implements malloc using a free list
+ ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-struct chunk {
+struct chunk
+{
   int size;
   int used;
   struct chunk *next;
 };
+
 struct chunk *flist = NULL;
 
-void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+void *malloc(size_t size)
+{
+  if (size == 0)
+  {
+    return NULL;
+  }
+
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  while (next != NULL)
+  {
+    if (next->size >= size)
+    {
+      if (prev != NULL)
+      {
+        prev->next = next->next;
+      }
+      else
+      {
+        flist = next->next;
+      }
+      next->used = size;
+      return (void *)(next + 1);
+    }
+    prev = next;
+    next = next->next;
+  }
+
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void *)-1)
+  {
+    return NULL;
+  }
+
+  struct chunk *new_chunk = (struct chunk *)memory;
+  new_chunk->size = size;
+  new_chunk->used = size;
+  new_chunk->next = NULL;
+  return (void *)(new_chunk + 1);
 }
 
-void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return;
+void free(void *memory)
+{
+  if (memory == NULL)
+  {
+    return;
+  }
+
+  struct chunk *free_chunk = (struct chunk *)((char *)memory - sizeof(struct chunk));
+  free_chunk->next = flist;
+  flist = free_chunk;
 }
